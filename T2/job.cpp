f681696f47eb4cb4bc937fcaabaab4c62cf21406
ClementeCW0 <clemente.wacquez@uc.cl>
2025-09-06 19:45:13 -0400

    Avance T2 (4)

--- T2/job.cpp
+++ T2/job.cpp
@@ -1,13 +1,15 @@
-#include "submatrix.h"
+#include "tarea2_read_matrix.h"
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main(){
-	int ndim = 2;
+	int my_firstcol = 2;
+	int my_ncols = 2;
 	double* A_ij;
-	A_ij = submatrix(3, ndim, 4, ndim);
-	for (int c = 0; c < ndim * ndim; c++){
+	A_ij = submatrix(my_firstcol, my_ncols);
+
+	for (int c = 0; c < my_firstcol * my_ncols; c++){
 		cout << c << " " << A_ij[c] << endl;
 	}
 	delete[] A_ij;
