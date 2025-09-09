124d43e116b6605aabe7eafd269eddd4c67c7238
ClementeCW0 <clemente.wacquez@uc.cl>
2025-09-09 19:01:55 -0300

    Actualización submit_py.sh (1)

--- T2/submit_py.sh
+++ T2/submit_py.sh
@@ -13,8 +13,9 @@
 echo "start script"
 date
 
-which python3
-time python3 tarea2_generate_matrix.py
+which python
+pytohn --version
+time python tarea2_generate_matrix.py
 
 echo "end script"
 date
