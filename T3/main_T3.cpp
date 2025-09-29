cf275e2c8306e3d60c9e06caf3c62c1ea7bafe69
ClementeCW0 <clemente.wacquez@uc.cl>
2025-09-29 19:31:10 -0300

    T3 (3)

--- T3/main_T3.cpp
+++ T3/main_T3.cpp
@@ -1,8 +1,72 @@
+#include <cmath>
 #include <omp.h>
 #include <iostream>
 using namespace std;
 
-float* random_matrix(int n){
+float alpha(float x, float y){
+	return x * (x - 1) * y * (y - 1) + 1;
+}
+float f(float x, float y){
+	float cx = 0.4;
+	float cy = 0.8;
+	float sx = 0.2;
+	float sy = 0.1;
+	return exp(-pow((x - cx) / sx, 2.0) / 2 -pow((y - cy) / sy, 2.0) / 2) / (2 * M_PI * sx * sy);
+}
+float tidy_alpha(int i, int j, int n, int k){
+	// Función para generar de forma ordenada los valores del stencil.
+	switch (k) {
+		case 0: // NORTE
+		return - n * n * alpha(i / n, (j + 0.5) / n);
+		break;
+
+		case 1: // SUR
+		return - n * n * alpha(i / n, (j - 0.5) / n);
+		break;
+
+		case 2: // ESTE
+		return - n * n * alpha((i + 0.5) / n, j / n);
+		break;
+
+		case 3: // OESTE
+		return - n * n * alpha((i - 0.5) / n, j / n);
+		break;
+
+		case 4: // CENTRO
+		return - n * n * (alpha((i - 0.5) / n, j / n) + alpha((i + 0.5) / n, j / n))
+		break;
+
+
+	}
+}
+float* get_stencil(int num_dividions) {
+	// Se asume que el stencil es de cruz con 5 elementos. Se creará un arreglo de tamaño
+	// 5 * num_divisoins \times num_divisions que representará los cinco arreglos del stencil
+	// en el orden [N S E W C]
+	float* stencil = (float*) calloc(5 * num_dividions * num_dividions, sizeof(float));
+	for (int k = 0; k < 5; k++){
+		switch (k) {
+			case 0:
+			for (int i = 0; i < num_dividions; i++){
+				for (int j = 0; j < num_dividions; j++){
+					stencil[
+				}
+			}
+		}
+      }
+
+
+	return stencil;
+}
+float* sparse_matvec(int n, int* offset, float* vec) {
+	float* result = (float*) calloc(n, sizeof(float));
+	#pragma omp parallel for
+	for (int i = 0; i < n * n; i++) {
+		}
+	}
+	return result;
+}
+float* sparse_matvec(int n){
 	float* mat = (float*) calloc(n, sizeof(float));
 
 
